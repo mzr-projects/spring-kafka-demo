91298d9d7b81146666a19fd571642419ce3ea9fa
Maziar Taghipour <taghipour.maziar@yahoo.com>
2022-12-13 11:10:14 -0800

    Initial commit

---
+++ src/test/java/com/mt/kafka/KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mt.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
